0b7ca8c52af967fcb9f42ccf466b1c8f91acee1c
David Taing <adavidtaing@gmail.com>
2022-03-23 10:59:55 +1100

    add className to form label elements

--- src/components/TenantDetailsForm.tsx
+++ src/components/TenantDetailsForm.tsx
@@ -42,32 +42,72 @@ function TenantDetailsForm() {
   return (
     <form onSubmit={formik.handleSubmit}>
       <h1>Tenant</h1>
-      <label htmlFor="preferredName">Preferred Name:</label>
-      <label htmlFor="fullname">Full Name:</label>
-      <label htmlFor="gender">Gender:</label>
-      <label htmlFor="dob">Date Of Birth:</label>
-      <label htmlFor="phone">Phone:</label>
-      <label htmlFor="mobile">Mobile:</label>
-      <label htmlFor="email">Email:</label>
+      <label className="form-label" htmlFor="preferredName">
+        Preferred Name:
+      </label>
+      <label className="form-label" htmlFor="fullname">
+        Full Name:
+      </label>
+      <label className="form-label" htmlFor="gender">
+        Gender:
+      </label>
+      <label className="form-label" htmlFor="dob">
+        Date Of Birth:
+      </label>
+      <label className="form-label" htmlFor="phone">
+        Phone:
+      </label>
+      <label className="form-label" htmlFor="mobile">
+        Mobile:
+      </label>
+      <label className="form-label" htmlFor="email">
+        Email:
+      </label>
 
       <h2>Rental History</h2>
-      <label htmlFor="address">Address:</label>
-      <label htmlFor="status">Rental Status:</label>
+      <label className="form-label" htmlFor="address">
+        Address:
+      </label>
+      <label className="form-label" htmlFor="status">
+        Rental Status:
+      </label>
       <h3>Reference</h3>
-      <label htmlFor="property-reference-name">Name:</label>
-      <label htmlFor="property-reference-email">Email:</label>
-      <label htmlFor="property-reference-phone">Phone:</label>
+      <label className="form-label" htmlFor="property-reference-name">
+        Name:
+      </label>
+      <label className="form-label" htmlFor="property-reference-email">
+        Email:
+      </label>
+      <label className="form-label" htmlFor="property-reference-phone">
+        Phone:
+      </label>
 
       <h2>Employment</h2>
-      <label htmlFor="employment-title">Job Title:</label>
-      <label htmlFor="employment-type">Type:</label>
-      <label htmlFor="employment-net-income">Net Income:</label>
-      <label htmlFor="employment-pay-period">Pay Period:</label>
+      <label className="form-label" htmlFor="employment-title">
+        Job Title:
+      </label>
+      <label className="form-label" htmlFor="employment-type">
+        Type:
+      </label>
+      <label className="form-label" htmlFor="employment-net-income">
+        Net Income:
+      </label>
+      <label className="form-label" htmlFor="employment-pay-period">
+        Pay Period:
+      </label>
       <h3>Reference</h3>
-      <label htmlFor="employment-reference-name">Name:</label>
-      <label htmlFor="employment-reference-position">Position:</label>
-      <label htmlFor="employment-reference-email">Email:</label>
-      <label htmlFor="employment-reference-phone">Phone:</label>
+      <label className="form-label" htmlFor="employment-reference-name">
+        Name:
+      </label>
+      <label className="form-label" htmlFor="employment-reference-position">
+        Position:
+      </label>
+      <label className="form-label" htmlFor="employment-reference-email">
+        Email:
+      </label>
+      <label className="form-label" htmlFor="employment-reference-phone">
+        Phone:
+      </label>
 
       <button type="submit">Submit</button>
     </form>
