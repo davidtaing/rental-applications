472f59b8e60994cbda84f7318a2e1c380b9c1e9d
David Taing <adavidtaing@gmail.com>
2022-03-23 10:57:06 +1100

    add form labels to TenantDetailsForm
    In preparation for full form implementation

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -1,11 +1,13 @@
 import OccupantsForm from "./OccupantsForm";
 import PropertyDetailsForm from "./PropertyDetailsForm";
+import TenantDetailsForm from "./TenantDetailsForm";
 
 function ApplicationForm() {
   return (
     <div>
       <PropertyDetailsForm />
       <OccupantsForm />
+      <TenantDetailsForm />
     </div>
   );
 }
--- src/components/TenantDetailsForm.tsx
+++ src/components/TenantDetailsForm.tsx
@@ -19,7 +19,6 @@ function TenantDetailsForm() {
         status: "",
         reference: {
           name: "",
-          position: "",
           email: "",
           phone: "",
         },
@@ -42,7 +41,35 @@ function TenantDetailsForm() {
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <div></div>
+      <h1>Tenant</h1>
+      <label htmlFor="preferredName">Preferred Name:</label>
+      <label htmlFor="fullname">Full Name:</label>
+      <label htmlFor="gender">Gender:</label>
+      <label htmlFor="dob">Date Of Birth:</label>
+      <label htmlFor="phone">Phone:</label>
+      <label htmlFor="mobile">Mobile:</label>
+      <label htmlFor="email">Email:</label>
+
+      <h2>Rental History</h2>
+      <label htmlFor="address">Address:</label>
+      <label htmlFor="status">Rental Status:</label>
+      <h3>Reference</h3>
+      <label htmlFor="property-reference-name">Name:</label>
+      <label htmlFor="property-reference-email">Email:</label>
+      <label htmlFor="property-reference-phone">Phone:</label>
+
+      <h2>Employment</h2>
+      <label htmlFor="employment-title">Job Title:</label>
+      <label htmlFor="employment-type">Type:</label>
+      <label htmlFor="employment-net-income">Net Income:</label>
+      <label htmlFor="employment-pay-period">Pay Period:</label>
+      <h3>Reference</h3>
+      <label htmlFor="employment-reference-name">Name:</label>
+      <label htmlFor="employment-reference-position">Position:</label>
+      <label htmlFor="employment-reference-email">Email:</label>
+      <label htmlFor="employment-reference-phone">Phone:</label>
+
+      <button type="submit">Submit</button>
     </form>
   );
 }
