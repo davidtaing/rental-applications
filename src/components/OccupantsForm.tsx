b8f4137e9023dee3281d1c1abb04358d2d5eddcb
David Taing <adavidtaing@gmail.com>
2022-03-22 22:41:14 +1100

    add ApplicationForm and OccupantsForm components

--- src/components/OccupantsForm.tsx
+++ src/components/OccupantsForm.tsx
@@ -14,16 +14,52 @@ function OccupantsForm() {
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="adults">Adults: </label>
-      <input id="adults" type="number" min="0" defaultValue="0" />
-      <label htmlFor="children">Children: </label>
-      <input id="children" type="number" min="0" defaultValue="0" />
-      <label htmlFor="dogs">Dogs: </label>
-      <input id="dogs" type="number" min="0" defaultValue="0" />
-      <label htmlFor="cat">Cats: </label>
-      <input id="cat" type="number" min="0" defaultValue="0" />
-      <label htmlFor="otherPets">Other Pets: </label>
-      <input id="otherPets" type="number" min="0" defaultValue="0" />
+      <h1>Occupants</h1>
+      <label htmlFor="adults">Adults:</label>
+      <input
+        id="adults"
+        name="adults"
+        type="number"
+        min="0"
+        defaultValue="0"
+        onChange={formik.handleChange}
+      />
+      <label htmlFor="children">Children:</label>
+      <input
+        id="children"
+        name="chidlren"
+        type="number"
+        min="0"
+        defaultValue="0"
+        onChange={formik.handleChange}
+      />
+      <label htmlFor="dogs">Dogs:</label>
+      <input
+        id="dogs"
+        name="dogs"
+        type="number"
+        min="0"
+        defaultValue="0"
+        onChange={formik.handleChange}
+      />
+      <label htmlFor="cat">Cats:</label>
+      <input
+        id="cat"
+        name="cats"
+        type="number"
+        min="0"
+        defaultValue="0"
+        onChange={formik.handleChange}
+      />
+      <label htmlFor="otherPets">Other Pets:</label>
+      <input
+        id="otherPets"
+        name="otherPets"
+        type="number"
+        min="0"
+        defaultValue="0"
+        onChange={formik.handleChange}
+      />
       <button type="submit">Submit</button>
     </form>
   );
