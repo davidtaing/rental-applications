aba4d43181978f311d78ce2d927f93403c8dd766
David Taing <adavidtaing@gmail.com>
2022-03-23 14:21:04 +1100

    refactor LabelledSelect into seperate component

---
+++ src/components/common/LabelledSelect/index.tsx
@@ -0,0 +1,31 @@
+import { SelectHTMLAttributes } from "react";
+
+interface LabelledSelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  labelText: string;
+  values: Array<string>;
+}
+
+function LabelledSelect({
+  id,
+  labelText,
+  values,
+  ...otherProps
+}: LabelledSelectProps) {
+  return (
+    <div className="labelled-select">
+      <label className="form-label" htmlFor={id}>
+        {labelText}
+      </label>
+      <select className="dropdown" id={id} {...otherProps}>
+        <option value="">Select a Value</option>
+        {values.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default LabelledSelect;
