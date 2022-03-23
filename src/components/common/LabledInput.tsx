bf53db85523f063a9bde8c4cb5291388d203d4f8
David Taing <adavidtaing@gmail.com>
2022-03-23 12:29:11 +1100

    add LabledInput component
    in preparation for refactor

---
+++ src/components/common/LabledInput.tsx
@@ -0,0 +1,27 @@
+import { ChangeEventHandler } from "react";
+
+interface LabledInputProps {
+  id: string;
+  name: string;
+  text: string;
+  handleChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+function LabledInput({ id, name, text, handleChange }: LabledInputProps) {
+  return (
+    <div className="labed-input">
+      <label className="form-label" htmlFor={id}>
+        {text}
+      </label>
+      <input
+        className="text-input"
+        id={id}
+        name={name}
+        type="text"
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+
+export default LabledInput;
