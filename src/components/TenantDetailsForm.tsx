531c14f0216c7fdf685190a4ae24cc145eeecde6
David Taing <adavidtaing@gmail.com>
2022-03-23 11:23:38 +1100

    add form input elements for property history

--- src/components/TenantDetailsForm.tsx
+++ src/components/TenantDetailsForm.tsx
@@ -116,25 +116,70 @@ function TenantDetailsForm() {
         type="email"
         onChange={formik.handleChange}
       />
-
       <h2>Rental History</h2>
-      <label className="form-label" htmlFor="address">
+      <label className="form-label" htmlFor="property-address">
         Address:
       </label>
-      <label className="form-label" htmlFor="status">
+      <input
+        className="text-input"
+        id="property-address"
+        name="property.address"
+        type="text"
+        onChange={formik.handleChange}
+      />
+      <label className="form-label" htmlFor="property-status">
         Rental Status:
       </label>
+      <select
+        className="dropdown"
+        id="property-status"
+        name="property.status"
+        onChange={formik.handleChange}
+      >
+        <option value="">Select a Value</option>
+        <option value="Renting with Agency">Renting with Agency</option>
+        <option value="Renting with Private Landlord">
+          Renting with Private Landlord
+        </option>
+        <option value="House/Room Share">House/Room Share</option>
+        <option value="Living with Parents">Living with Parents</option>
+        <option value="Living in Own Property">Living in Own Property</option>
+        <option value="Just Arrived From Overseas">
+          Just Arrived From Overseas
+        </option>
+        <option value="Living Overseas">Living Overseas</option>
+      </select>
       <h3>Reference</h3>
       <label className="form-label" htmlFor="property-reference-name">
         Name:
       </label>
+      <input
+        className="text-input"
+        id="property-reference-name"
+        name="property.reference.name"
+        type="text"
+        onChange={formik.handleChange}
+      />
       <label className="form-label" htmlFor="property-reference-email">
         Email:
       </label>
+      <input
+        className="text-input"
+        id="property-reference-email"
+        name="property.reference.email"
+        type="email"
+        onChange={formik.handleChange}
+      />
       <label className="form-label" htmlFor="property-reference-phone">
         Phone:
       </label>
-
+      <input
+        className="text-input"
+        id="property-reference-phone"
+        name="property.reference.phone"
+        type="text"
+        onChange={formik.handleChange}
+      />
       <h2>Employment</h2>
       <label className="form-label" htmlFor="employment-title">
         Job Title:
@@ -161,7 +206,6 @@ function TenantDetailsForm() {
       <label className="form-label" htmlFor="employment-reference-phone">
         Phone:
       </label>
-
       <button type="submit">Submit</button>
     </form>
   );
