c8e07cf231b1e7994dbbe56b7a5549ecd228441e
David Taing <adavidtaing@gmail.com>
2022-03-24 22:42:53 +1100

    refactor: moved PropertyHistoryItem to separate file

--- src/features/TenantDetailsForm/PropertyHistoryList.tsx
+++ src/features/TenantDetailsForm/PropertyHistoryList.tsx
@@ -1,67 +1,9 @@
 import { FieldArray } from "formik";
-import { ChangeEventHandler, MouseEventHandler } from "react";
+import { ChangeEventHandler } from "react";
 
-import LabelledInput from "../../components/common/LabelledInput";
-import RentalStatusSelect from "../../components/RentalStatusSelect";
 import PropertyHistory from "../../types/PropertyHistory";
 import { createPropertyHistory } from "../../utils";
-
-interface PropertyHistoryItemProps {
-  tenantIndex: number;
-  index: number;
-  onChangeHandler: ChangeEventHandler;
-  onRemoveHandler: MouseEventHandler<HTMLButtonElement>;
-}
-
-function PropertyHistoryItem({
-  tenantIndex,
-  index,
-  onChangeHandler,
-  onRemoveHandler,
-}: PropertyHistoryItemProps) {
-  return (
-    <div className="property-listitem">
-      <h2>Property</h2>
-      <LabelledInput
-        id="property-address"
-        name={`tenants[${tenantIndex}].propertyHistory[${index}].address`}
-        type="text"
-        labelText="Address:"
-        onChange={onChangeHandler}
-      />
-      <RentalStatusSelect
-        id="property-status"
-        name={`tenants[${tenantIndex}].propertyHistory[${index}].status`}
-        onChange={onChangeHandler}
-      />
-      <h3>Reference</h3>
-      <LabelledInput
-        id="property-reference-name"
-        name={`tenants[${tenantIndex}].propertyHistory[${index}].reference.name`}
-        type="text"
-        labelText="Name:"
-        onChange={onChangeHandler}
-      />
-      <LabelledInput
-        id="property-reference-email"
-        name={`tenants[${tenantIndex}].propertyHistory[${index}].reference.email`}
-        type="email"
-        labelText="Email:"
-        onChange={onChangeHandler}
-      />
-      <LabelledInput
-        id="property-reference-phone"
-        name={`tenants[${tenantIndex}].propertyHistory[${index}].reference.phone`}
-        type="text"
-        labelText="Phone:"
-        onChange={onChangeHandler}
-      />
-      {index > 0 ? (
-        <button onClick={onRemoveHandler}>Remove History</button>
-      ) : null}
-    </div>
-  );
-}
+import { PropertyHistoryItem } from "./PropertyHistoryItem";
 
 interface PropertyHistoryListProps {
   tenantIndex: number;
