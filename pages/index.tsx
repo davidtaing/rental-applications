00061c3a66f41d075e2eefe3eec63e10f843d9cb
David Taing <adavidtaing@gmail.com>
2022-03-31 21:58:45 +1100

    remove ApplicationForm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import type { NextPage } from "next";
-import ApplicationForm from "../src/features/ApplicationForm";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <main>
-        <ApplicationForm />
-      </main>
+      <main></main>
     </div>
   );
 };
