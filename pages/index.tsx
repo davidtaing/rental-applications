60211f8e0fd2262cb5bc44f7fda7d2241781bc76
David Taing <adavidtaing@gmail.com>
2022-04-10 20:04:28 +1000

    fix: Element type is invalid from broken import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import LeaseDetailsFormSegment from "../src/features/LeaseDetailsFormSegment";
+import { LeaseDetailsFormSegment } from "../src/features/LeaseDetailsFormSegment";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
