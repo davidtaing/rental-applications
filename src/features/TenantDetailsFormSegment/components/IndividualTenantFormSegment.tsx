a483baeae9b253d5fb324a5031fdcafc9f835093
David Taing <adavidtaing@gmail.com>
2022-04-15 15:45:31 +1000

    fix: broken formik references
    in IndividualTenantFormSegment

--- src/features/TenantDetailsFormSegment/components/IndividualTenantFormSegment.tsx
+++ src/features/TenantDetailsFormSegment/components/IndividualTenantFormSegment.tsx
@@ -16,42 +16,42 @@ export function IndividualTenantFormSegment({ index }: Props) {
       <LabelledInput
         labelText="Full Name:"
         id="fullname"
-        name={`tenants${index}.fullname`}
+        name={`tenants[${index}].fullname`}
         type="text"
       />
       {/* Gender */}
       <LabelledSelect
         labelText="Gender: "
         id="gender"
-        name={`tenants${index}.gender`}
+        name={`tenants[${index}].gender`}
         values={["Male", "Female", "Other"]}
       />
       {/* Date of Birth */}
       <LabelledInput
         labelText="Date of Birth:"
         id="dob"
-        name={`tenants${index}.dob`}
+        name={`tenants[${index}].dob`}
         type="date"
       />
       {/* Mobile */}
       <LabelledInput
         labelText="Mobile:"
         id="mobile"
-        name={`tenants${index}.mobile`}
+        name={`tenants[${index}].mobile`}
         type="text"
       />
       {/* Phone */}
       <LabelledInput
         labelText="Phone:"
         id="phone"
-        name={`tenants${index}.phone`}
+        name={`tenants[${index}].phone`}
         type="text"
       />
       {/* Email */}
       <LabelledInput
         labelText="Email:"
         id="email"
-        name={`tenants${index}.email`}
+        name={`tenants[${index}].email`}
         type="email"
       />
     </div>
