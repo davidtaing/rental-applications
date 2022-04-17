4ffef30894e5260047b8fd2c67e3fd3b312fb7c3
David Taing <adavidtaing@gmail.com>
2022-04-17 12:29:41 +1000

    feat: RentalHistoryFormSegment
    add RentalStatus enum

--- src/features/RentalHistoryFormSegment/types/index.ts
+++ src/features/RentalHistoryFormSegment/types/index.ts
@@ -1,3 +1,14 @@
+export enum RentalStatus {
+  AGENCY_RENTAL = "Renting with Agency",
+  PRIVATE_RENTAL = "Renting with Private Landlord",
+  LIVING_WITH_PARENTS = "Living with Parents",
+  LIVING_IN_OWN_PROPERTY = "Living in Own Property",
+  SHARING = "Sharing",
+  CURRENTLY_OVERSEAS = "Currently Overseas",
+  JUST_ARRIVED_FROM_OVERSEAS = "Just Arrived from Overseas",
+  OHTER = "Other",
+}
+
 export interface RentalHistory {
   address: string;
   status: string;
