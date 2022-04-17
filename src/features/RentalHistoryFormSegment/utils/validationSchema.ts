7a9feeb5ddf1f63990a781afcac812623e272787
David Taing <adavidtaing@gmail.com>
2022-04-17 17:35:20 +1000

    feat: RentalHistoryForm validationSchema

---
+++ src/features/RentalHistoryFormSegment/utils/validationSchema.ts
@@ -0,0 +1,24 @@
+import * as Yup from "yup";
+import { PaymentFrequency } from "../../../types/PaymentFrequency";
+import { RentalReferenceType, RentalStatus } from "../types";
+
+const rentalHistoryValidationSchema = Yup.object().shape({
+  address: Yup.string().required(),
+  status: Yup.mixed<RentalStatus>().oneOf(Object.values(RentalStatus)),
+  rent: Yup.number().required().positive().integer(),
+  frequency: Yup.mixed<PaymentFrequency>().oneOf(
+    Object.values(PaymentFrequency)
+  ),
+  reference: Yup.object().shape({
+    name: Yup.string().required(),
+    status: Yup.mixed<RentalReferenceType>().oneOf(
+      Object.values(RentalReferenceType)
+    ),
+    phone: Yup.string().required(),
+    email: Yup.string().email(),
+  }),
+});
+
+export const rentalHistoryFormValidationSchema = Yup.object().shape({
+  rentalHistory: Yup.array().of(rentalHistoryValidationSchema),
+});
