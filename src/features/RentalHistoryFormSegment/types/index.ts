6ab6841bab16108750b7ad9fade96bd6183a5e31
David Taing <adavidtaing@gmail.com>
2022-04-17 12:10:37 +1000

    feat: RentalHistoryForm
    - add RentalHistory interface

---
+++ src/features/RentalHistoryFormSegment/types/index.ts
@@ -0,0 +1,12 @@
+export interface RentalHistory {
+  address: string;
+  status: string;
+  rent: number;
+  frequency: string;
+  reference: {
+    name: string;
+    type: string;
+    phone: string;
+    email: string;
+  };
+}
