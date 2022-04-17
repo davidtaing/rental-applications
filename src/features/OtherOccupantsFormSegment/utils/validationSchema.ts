0976b02d2e88fa8b04ad3d17c8a6e2331d2b909e
David Taing <adavidtaing@gmail.com>
2022-04-17 17:19:47 +1000

    feat: OtherOccupantsForm validation

--- src/features/OtherOccupantsFormSegment/utils/validationSchema.ts
+++ src/features/OtherOccupantsFormSegment/utils/validationSchema.ts
@@ -1,12 +1,15 @@
 import * as Yup from "yup";
+import { OtherOccupantRelation } from "../types";
 
 export const validationSchema = Yup.object().shape({
   otherOccupants: Yup.array().of(
     Yup.object().shape({
-      // fullname
-      // relation
-      // age
-      // gender
+      fullname: Yup.string().required(),
+      relation: Yup.mixed<OtherOccupantRelation>()
+        .oneOf(Object.values(OtherOccupantRelation))
+        .required(),
+      age: Yup.number().required().positive().integer(),
+      gender: Yup.string().required(),
     })
   ),
 });
