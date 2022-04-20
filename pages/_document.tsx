3cf6481efb8bd79a13f30e426d37a6aed6f3f1b4
David Taing <adavidtaing@gmail.com>
2022-04-20 12:53:20 +1000

    style: globals.css body font & header sizes
    - body fonts is now Open Sans and
    - adjustheader sizes based on IBM Carbon Design System

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,7 +18,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&family=Poppins:wght@300;400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
             rel="stylesheet"
           />
         </Head>
