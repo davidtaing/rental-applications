03d30282068144a8a0a2a06d167c3aab3611eaa8
David Taing <adavidtaing@gmail.com>
2022-04-25 00:35:35 +1000

    feat: LabelledSelect
    add otherProps spread to allow customizing input props

--- src/components/common/LabelledSelectRHF.tsx
+++ src/components/common/LabelledSelectRHF.tsx
@@ -1,17 +1,24 @@
+import { SelectHTMLAttributes } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
 
-interface Props {
+interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
   key: string;
   registerReturn: UseFormRegisterReturn;
   values: string[];
   label: string;
 }
 
-export function LabelledSelect({ key, registerReturn, values, label }: Props) {
+export function LabelledSelect({
+  key,
+  registerReturn,
+  values,
+  label,
+  ...otherProps
+}: Props) {
   return (
     <div className="labelledSelect">
       <label>{label}</label>
-      <select key={key} {...registerReturn}>
+      <select key={key} {...registerReturn} {...otherProps}>
         <option value="">Select a Value</option>
         {values.map((item, index) => (
           <option key={index} value={item}>
