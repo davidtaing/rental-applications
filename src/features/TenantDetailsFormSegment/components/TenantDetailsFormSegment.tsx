f9992d7b2371a3560f6547a9308387b8d5c78575
David Taing <adavidtaing@gmail.com>
2022-04-25 00:16:33 +1000

    feat: TenantDetailsForm
    Summary:
    - convert form to react-hook-form due to performance issues with Formik

--- src/features/TenantDetailsFormSegment/components/TenantDetailsFormSegment.tsx
+++ src/features/TenantDetailsFormSegment/components/TenantDetailsFormSegment.tsx
@@ -1,35 +1,64 @@
-import { FieldArray, useFormikContext } from "formik";
-import { FieldArrayFormSegment } from "../../../components/common/FieldArrayFormSegment";
-import { Tenant } from "../types";
-import { createTenant } from "../utils";
-import { TenantItem } from "./TenantItem";
+import { useFieldArray, useForm } from "react-hook-form";
+import { TenantDetailsFormState } from "../types";
+import { initTenantDetailsFormState, createTenant } from "../utils";
 
 /**
  * Form Segment for Rent-Paying Occupants
  */
 export function TenantDetailsFormSegment() {
-  const {
-    values: { tenants },
-  } = useFormikContext();
+  const { control, register } = useForm<TenantDetailsFormState>({
+    defaultValues: initTenantDetailsFormState(),
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "tenants",
+  });
 
   return (
     <div className="tenantDetailsForm formSegment">
       <h1>Tenant Details</h1>
-      <FieldArrayFormSegment
-        values={tenants}
-        formikReference="tenants"
-        createItemFn={createTenant}
-        buttonText="Tenant"
-        minItems={1}
-      >
-        {tenants.map((item: Tenant, idx: string) => (
-          <TenantItem
-            key={idx}
-            formikReference={`tenants[${idx}]`}
-            data={item}
+      {fields.map((field, index) => (
+        <div key={field.id}>
+          <h2>Tenant</h2>
+          <label>Fullname:</label>
+          <input key={field.id} {...register(`tenants.${index}.fullname`)} />
+          <label>Gender:</label>
+          <input key={field.id} {...register(`tenants.${index}.gender`)} />
+          <label>Date of Birth:</label>
+          <input
+            type="date"
+            key={field.id}
+            {...register(`tenants.${index}.dob`)}
           />
-        ))}
-      </FieldArrayFormSegment>
+          <label>Mobile:</label>
+          <input key={field.id} {...register(`tenants.${index}.mobile`)} />
+          <label>Phone:</label>
+          <input key={field.id} {...register(`tenants.${index}.phone`)} />
+          <label>Email:</label>
+          <input key={field.id} {...register(`tenants.${index}.email`)} />
+
+          <h3>Employment</h3>
+          <h3>Income Support Payments</h3>
+          <h3>Rental Income</h3>
+          <h3>Rental History</h3>
+        </div>
+      ))}
+      <div className="fieldArray--btn-container">
+        <button
+          className="btn--secondary-outline"
+          type="button"
+          onClick={() => append(createTenant())}
+        >
+          Add Tenant
+        </button>
+        <button
+          className="btn--danger-outline"
+          type="button"
+          onClick={() => remove(fields.length - 1)}
+        >
+          Remove Tenant
+        </button>
+      </div>
     </div>
   );
 }
