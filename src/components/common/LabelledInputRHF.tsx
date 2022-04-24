fdcfe65cd593e4cba146e59ab12554be2e1f9d31
David Taing <adavidtaing@gmail.com>
2022-04-25 00:27:13 +1000

    feat: LabelledInput
    add otherProps spread to allow customizing input props

--- src/components/common/LabelledInputRHF.tsx
+++ src/components/common/LabelledInputRHF.tsx
@@ -1,16 +1,22 @@
+import { FieldAttributes } from "formik";
 import { UseFormRegisterReturn } from "react-hook-form";
 
-interface Props {
+interface Props extends FieldAttributes<any> {
   key: string;
   registerReturn: UseFormRegisterReturn;
   label: string;
 }
 
-export function LabelledInput({ key, registerReturn, label }: Props) {
+export function LabelledInput({
+  key,
+  registerReturn,
+  label,
+  ...otherProps
+}: Props) {
   return (
     <div className="labelledInput">
       <label className="input__label">{label}</label>
-      <input key={key} {...registerReturn} />
+      <input key={key} {...registerReturn} {...otherProps} />
       <span className="input__error">Error: Invalid Input</span>
     </div>
   );
