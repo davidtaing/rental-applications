132bf40ec7010be7889537658e992f153774de44
David Taing <adavidtaing@gmail.com>
2022-04-25 18:30:01 +1000

    test: OtherOccuapntsFormSegment
    Summary:
    - renders 'Add Occupant' button
    - does not render 'Remove Occupant' button
    - renders Occupant header when 'Add Occupant' button is clicked
    - renders 'Remove Occupant' button when 'Add Occupant' button is clicked

---
+++ src/features/OtherOccupantsFormSegment/test/OtherOccupantsFormSegment.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { OtherOccupantsFormSegment } from "../components/OtherOccupantsFormSegment";
+
+test("renders 'Add Occupant' button", async () => {
+  render(<OtherOccupantsFormSegment />);
+  const addOccupantButton = screen.getByText(/^add occupant$/i);
+
+  expect(addOccupantButton).toBeTruthy();
+});
+
+test("does not render 'Remove Occupant' button", async () => {
+  render(<OtherOccupantsFormSegment />);
+  const addOccupantButton = screen.queryByText(/^remove occupant$/i);
+
+  expect(addOccupantButton).toBeFalsy();
+});
+
+test("renders Occupant header when 'Add Occupant' button is clicked", async () => {
+  render(<OtherOccupantsFormSegment />);
+  const addOccupantButton = screen.getByText(/^add occupant$/i);
+
+  let occupantHeader = screen.queryByText(/^occupant$/i);
+  expect(occupantHeader).toBeFalsy();
+
+  await userEvent.click(addOccupantButton);
+
+  let occupantHeaders = screen.queryAllByText(/^occupant$/i);
+  expect(occupantHeaders.length).toBe(1);
+});
+
+test("renders 'Remove Occupant' button when 'Add Occupant' button is clicked", async () => {
+  render(<OtherOccupantsFormSegment />);
+  const addOccupantButton = screen.getByText(/^add occupant$/i);
+
+  await userEvent.click(addOccupantButton);
+
+  let removeOccupantButton = screen.getByText(/^remove occupant$/i);
+  expect(removeOccupantButton).toBeTruthy();
+});
