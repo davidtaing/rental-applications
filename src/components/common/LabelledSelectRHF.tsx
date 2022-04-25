aca12a506406b869d327fe6e04f839716d3a6d13
David Taing <adavidtaing@gmail.com>
2022-04-25 10:48:08 +1000

    feat: LabelledSelect
    made fieldId field optional

--- src/components/common/LabelledSelectRHF.tsx
+++ src/components/common/LabelledSelectRHF.tsx
@@ -2,7 +2,7 @@ import { SelectHTMLAttributes } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
 
 interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
-  fieldId: string;
+  fieldId?: string;
   registerReturn: UseFormRegisterReturn;
   values: string[];
   label: string;
@@ -18,10 +18,8 @@ export function LabelledSelect({
   return (
     <div className="labelledSelect">
       <label className="input__label">{label}</label>
-      <select key={fieldId} {...registerReturn} {...otherProps}>
-        <option value="" selected>
-          Select a Value
-        </option>
+      <select key={fieldId} {...registerReturn} {...otherProps} defaultValue="">
+        <option value="">Select a Value</option>
         {values.map((item, index) => (
           <option key={index} value={item}>
             {item}
