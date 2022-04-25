c59c01552f351c8567a7a74559fafc4ac9b98fc3
David Taing <adavidtaing@gmail.com>
2022-04-25 12:01:22 +1000

    remove IncomeFormSegment

--- src/features/IncomeFormSegment/index.tsx
+++ src/features/IncomeFormSegment/index.tsx
@@ -1,6 +1,5 @@
 export * from "./components/EmploymentItem";
 export * from "./components/EmploymentHistoryFormSegment";
-export * from "./components/IncomeFormSegment";
 export * from "./components/IncomeSupportFormSegment";
 export * from "./components/RealEstateIncomeFormSegment";
 export * from "./types";
