d259d94598216ad17765d74a9c161c1d72f215dc
David Taing <adavidtaing@gmail.com>
2022-04-25 12:16:48 +1000

    refactor: remove fieldId prop

--- src/components/common/GenderSelectRHF.tsx
+++ src/components/common/GenderSelectRHF.tsx
@@ -3,14 +3,12 @@ import { Gender } from "../../types/Gender";
 import { LabelledSelect } from "./LabelledSelectRHF";
 
 interface Props {
-  fieldId?: string;
   registerReturn: UseFormRegisterReturn;
 }
 
-export function GenderSelect({ fieldId, registerReturn }: Props) {
+export function GenderSelect({ registerReturn }: Props) {
   return (
     <LabelledSelect
-      fieldId={fieldId}
       registerReturn={registerReturn}
       values={Object.values(Gender)}
       label={"Gender:"}
