4cfb64e6dac1ca4a25bb7fc614d9abe0f5887e29
David Taing <adavidtaing@gmail.com>
2022-04-25 18:35:45 +1000

    test: PetsFormSegment
    Summary:
    - renders 'Add Pet' button,
    - does not render 'Remove Pet' button,
    - renders Pets header when 'Add Pet' button is clicked,
    - renders 'Remove Occupant' button when 'Add Occupant' button is clicked

---
+++ src/features/PetsFormSegment/test/PetsFormSegment.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { PetsFormSegment } from "../components/PetsFormSegment";
+
+test("renders 'Add Pet' button", async () => {
+  render(<PetsFormSegment />);
+  const addPetsButton = screen.getByText(/^add pet$/i);
+
+  expect(addPetsButton).toBeTruthy();
+});
+
+test("does not render 'Remove Pet' button", async () => {
+  render(<PetsFormSegment />);
+  const addPetsButton = screen.queryByText(/^remove pet$/i);
+
+  expect(addPetsButton).toBeFalsy();
+});
+
+test("renders Pets header when 'Add Pet' button is clicked", async () => {
+  render(<PetsFormSegment />);
+  const addPetButton = screen.getByText(/^add pet$/i);
+
+  let petsHeader = screen.queryByText(/^pets$/i);
+  expect(petsHeader).toBeFalsy();
+
+  await userEvent.click(addPetButton);
+
+  let petsHeaders = screen.queryAllByText(/^pets$/i);
+  expect(petsHeaders.length).toBe(1);
+});
+
+test("renders 'Remove Occupant' button when 'Add Occupant' button is clicked", async () => {
+  render(<PetsFormSegment />);
+  const addPetsButton = screen.getByText(/^add pet$/i);
+
+  await userEvent.click(addPetsButton);
+
+  let removePetsButton = screen.getByText(/^remove pet$/i);
+  expect(removePetsButton).toBeTruthy();
+});
