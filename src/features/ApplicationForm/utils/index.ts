4640ea14f7ffa68a2dd40d23aceffa115c8390e7
David Taing <adavidtaing@gmail.com>
2022-04-26 23:43:47 +1000

    feat: ApplicationForm context
    Summary:
    - Add ApplicationForm component to look after entire form state.
    - Forms will propogate state when prev or next buttons are clicked.

--- src/features/ApplicationForm/utils/index.ts
+++ src/features/ApplicationForm/utils/index.ts
@@ -2,12 +2,13 @@ import { initLeaseDetailsFormState } from "../../LeaseDetailsFormSegment";
 import { initOtherOccupantsFormState } from "../../OtherOccupantsFormSegment";
 import { initPetsFormState } from "../../PetsFormSegment";
 import { initTenantDetailsFormState } from "../../TenantDetailsFormSegment";
+import { ApplicationFormState } from "../types/ApplicationFormState";
 
-export const initApplicationFormState = () => {
+export const initApplicationFormState = (): ApplicationFormState => {
   return {
-    ...initLeaseDetailsFormState,
-    ...initTenantDetailsFormState,
-    ...initOtherOccupantsFormState,
-    ...initPetsFormState,
+    ...initLeaseDetailsFormState(),
+    ...initTenantDetailsFormState(),
+    ...initOtherOccupantsFormState(),
+    ...initPetsFormState(),
   };
 };
